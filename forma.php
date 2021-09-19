01961799d9c34ee33b237ac291a93c9b99b0194c
Stas Nykytiuk <dev@rivne.itstep.org>
2021-09-19 16:23:32 +0300

    20 - PHP-Список зареєстрованих

--- forma.php
+++
@@ -1,9 +0,0 @@
-<h1 align="center">Дякуємо за реєстрацію!</h1>
-<?php
-$st = $_POST['name2'].";".$_POST['name1'].";".$_POST['nicl'].";".$_POST['email'].";".$_POST['password']."\n";
-
-$fp = fopen("baza.txt","a");
-$test = fwrite($fp,$st);
-echo "<h2 align='center' >Ви ввели:".$_POST['name2'].",".$_POST['name1'].",".$_POST['nicl'].",".$_POST['email'].",".$_POST['password']."</h2>";
-?>
-</h1>
